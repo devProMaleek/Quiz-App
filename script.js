15bfc39f8b3de0b4597ca40df84740bad87123b3
devProMaleek <milikiadbay@gmail.com>
2021-08-08 23:44:48 +0100

    Third Commit: Completed a little,  logic on the quiz app

--- script.js
+++ script.js
@@ -35,17 +35,20 @@ const quizData = [
   },
 ];
 
+// Get the element of the answer.
+const answerEls = document.querySelectorAll("answer");
 // Get all the Element, Initialize the variables
-
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById('submit');
+const submitBtn = document.getElementById("submit");
 
 // Initialize the index of the Quiz.
 let currentQuiz = 0;
+// Initialize the value of the score
+let score = 0;
 
 // Call function
 loadQuiz();
@@ -53,6 +56,8 @@ loadQuiz();
 // Load Quiz Function
 
 function loadQuiz() {
+  // Call the deselect Function.
+  deselectAnswers();
   // Calling the index of the next quiz.
   const currentQuizData = quizData[currentQuiz];
   // Insert the Quiz and option to the element
@@ -61,11 +66,45 @@ function loadQuiz() {
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
-
-
 }
 
-submitBtn.addEventListener('click', () => {
-    currentQuiz++;
+// Function to get the clicked option.
+function getSelected() {
+  // Initialize the value of answer.
+  let answer = undefined;
+
+  // Get the id of the selected option
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  console.log(answerEl.che);
+  return answer;
+}
+
+// Function to deselect the option selected.
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+    console.log(answer);
+  });
+}
+
+submitBtn.addEventListener("click", () => {
+  // Check to see the answer
+  const answer = getSelected();
+
+  // Increase score if the answer is correct
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score += 10;
+    }
+  }
+  // Increase the index of the quiz data, so as to load the next question.
+  currentQuiz++;
+  // Load the next question
+  if (currentQuiz < quizData.length) {
     loadQuiz();
-})
+  }
+});
