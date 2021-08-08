dd9890c1a4369409ab52730a1b0cf028699c57e2
devProMaleek <milikiadbay@gmail.com>
2021-08-08 13:48:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Defining the the  Quiz data.
+const quizData = [
+  {
+    question: "What is the most used programming language in 2020?",
+    a: "Java",
+    b: "Python",
+    c: "C",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question:
+      'Which of the following is not one of the early "protocols", or ways to use the Internet ?',
+    a: "Blogging",
+    b: "Telnet",
+    c: "Gopher",
+    d: "File Transfer Protocol (FTP)",
+    correct: "a",
+  },
+    {
+        question: "Who invented flexible photographic film",
+        a: "Leonardo da Vinci",
+        b: "Linda Eastman",
+        c: "Louis Daguerre",
+        d: "George Eastman",
+        correct: "d",
+    }, {
+        question: "Which of this is a file format for digital images",
+        a: "CIA",
+        b: "JPG",
+        c: "ICBM",
+        d: "IBM",
+        correct: "b",
+  }
+];
